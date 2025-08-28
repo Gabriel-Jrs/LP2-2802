85cf48345544acc72c1770e8fee3a5318da97239
Gabriel José <gabriell4924462@gmail.com>
2025-08-28 15:31:11 -0300

    7845

---
+++ explo.js
@@ -0,0 +1,20 @@
+// contatyor 10 ate 1
+
+let x = 10
+
+while(x >= 1){
+    console.log(x)
+    x--
+}
+
+x = 10
+
+console.log("Do while")
+do{
+    console.log(x)
+    x--
+}while( x >=1 )
+console.log("For")
+for(x=10; x>=1; x--){
+    console.log(x)
+}
---
+++ q1.js
@@ -0,0 +1,13 @@
+/*Escreva um algoritmo que receba os valores dos
+salários de todos os membros de uma família e ao
+final, mostre quanto dinheiro a família possui.
+*/
+let total = 0
+let salario = 1
+
+while (salario != 0.0) {
+  salario = prompt("Digite o salário do membro da família (ou 0 para encerrar):")
+  salario = Number
+  total = total + salario
+}
+
