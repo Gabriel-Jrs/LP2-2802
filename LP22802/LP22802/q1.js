46b7799445593a9dc8c7e26d69c74e0e0855c151
Gabriel José <gabriell4924462@gmail.com>
2025-09-03 12:37:01 -0300

    eghu

---
+++ LP22802/LP22802/q1.js
@@ -0,0 +1,9 @@
+let total = 0
+let salario = 1
+
+while (salario != 0.0) {
+  salario = prompt("Digite o salário do membro da família (ou 0 para encerrar):")
+  salario = Number
+  total = total + salario
+}
+
