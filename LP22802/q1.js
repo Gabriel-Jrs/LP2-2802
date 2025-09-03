77fd484021ab9b0028d38ecbaee9f0d1cf469d78
Gabriel José <gabriell4924462@gmail.com>
2025-09-03 12:38:20 -0300

    Delete LP22802 directory

--- LP22802/q1.js
+++
@@ -1,9 +0,0 @@
-let total = 0
-let salario = 1
-
-while (salario != 0.0) {
-  salario = prompt("Digite o salário do membro da família (ou 0 para encerrar):")
-  salario = Number
-  total = total + salario
-}
-
